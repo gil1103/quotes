b1d9525aca89cb0e281b7870b34a58e99a3d5e03
Gil <Gilhershk11@gmail.com>
2021-08-03 09:42:41 +0300

    update FIREBASE_DOMAIN

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,5 +1,4 @@
-const FIREBASE_DOMAIN =
-	'https://food-order-app-7cec7-default-rtdb.firebaseio.com';
+const FIREBASE_DOMAIN = 'https://react-http-99f38-default-rtdb.firebaseio.com/';
 
 export async function getAllQuotes() {
   const response = await fetch(`${FIREBASE_DOMAIN}/quotes.json`);
